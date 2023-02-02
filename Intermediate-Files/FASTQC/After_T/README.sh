e6b40de152e231bea30ee9c30306a271b0df3316
enyaenara <enya_mar@hotmail.com>
2023-02-02 14:01:47 -0600

    Archivos intermedios

--- Intermediate-Files/FASTQC/After_T/README.sh
+++
@@ -1,3 +0,0 @@
-#En esta carpeta se guardaran los archivos resultantes del Script 04
-#Esto es para analizar los módulos de calidad posteriores al Trimming
-#De esta manera, determinar que las secuencias tengan calidad adecuada
